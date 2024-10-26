64b6206725838b2f87502994976c16ed1ff12c0f
frankois <dabonot.francois@gmail.com>
2024-10-26 17:36:07 +0200

    Fixing Firebase loading on iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,14 +3,13 @@ import FirebaseCore
 
 // For full explanation
 // https://firebase.google.com/docs/ios/learn-more?hl=en#swiftui
-
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+        FirebaseApp.configure()
+        return true
+    }
 }
 
 @main
@@ -18,10 +17,6 @@ struct iOSApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    init() {
-      FirebaseApp.configure()
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
