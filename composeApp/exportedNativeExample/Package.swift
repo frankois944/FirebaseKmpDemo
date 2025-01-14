4045014b65dcaa3dede403fe3896e18f5e33fa0f
frankois <dabonot.francois@gmail.com>
2025-01-14 10:21:27 +0100

    migration to spm4kmp
    remove cocoapods

---
+++ composeApp/exportedNativeExample/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+  name: "exportedNativeExample",
+  platforms: [.iOS("12.0"), .macOS("10.13"), .tvOS("12.0"), .watchOS("4.0")],
+  products: [
+    .library(
+      name: "exportedNativeExample",
+      type: .static,
+      targets: ["exportedNativeExample"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.6.0")
+  ],
+  targets: [
+    .target(
+      name: "exportedNativeExample",
+      dependencies: [
+        .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseFunctions", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseInstallations", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
+        .product(name: "FirebasePerformance", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
+      ],
+      path: "Sources")
+
+  ]
+)
