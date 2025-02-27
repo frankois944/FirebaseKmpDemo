eed8d2aeb2d3fc7d692d60d4def4af9884786c23
frankois <dabonot.francois@gmail.com>
2025-02-27 09:36:08 +0100

    fix android build

--- composeApp/src/androidMain/kotlin/fr/francoisdabonot/firebasekmpdemo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/fr/francoisdabonot/firebasekmpdemo/MainActivity.kt
@@ -1,13 +1,10 @@
 package fr.francoisdabonot.firebasekmpdemo
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import dev.gitlive.firebase.FirebaseOptions
-import fr.francoisdabonot.firebasekmpdemo.firebase.startFirebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
