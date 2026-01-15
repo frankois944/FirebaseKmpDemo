a1c18fa70a2b0e7f1c4febe7c2fd5c384a04e16c
frankois <dabonot.francois@gmail.com>
2026-01-15 08:09:50 +0100

    update plugin deps

--- composeApp/exportedNativeExample/Package.swift
+++ composeApp/exportedNativeExample/Package.swift
@@ -1,28 +1,29 @@
+
 // swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-  name: "exportedNativeExample",
-  platforms: [.iOS("12.0"), .macOS("10.13"), .tvOS("12.0"), .watchOS("4.0")],
-  products: [
-    .library(
-      name: "exportedNativeExample",
-      type: .static,
-      targets: ["exportedNativeExample"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.6.0")
-  ],
-  targets: [
-    .target(
-      name: "exportedNativeExample",
-      dependencies: [
-        .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
-        .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
-      ],
-      path: "Sources"
-
-    )
-
-  ]
+    name: "exportedNativeExample",
+    platforms: [.iOS("12.0"),.macOS("10.13"),.tvOS("12.0"),.watchOS("4.0")],
+    products: [
+        .library(
+            name: "exportedNativeExample",
+            type: .static,
+            targets: ["exportedNativeExample"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "11.6.0")
+    ],
+    targets: [
+        .target(
+            name: "exportedNativeExample",
+            dependencies: [
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),.product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
+            ],
+            path: "Sources"
+            
+        )
+        
+    ]
 )
+        
